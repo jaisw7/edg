652b19ecaf587b11f14dcaeee25e6f78206c8e50
Shashank Jaiswal <jaisw7@gmail.com>
2025-01-31 03:08:42 -0500

    Do not track torch gradients globally

--- edgfs2D/post_process/initialize.py
+++ edgfs2D/post_process/initialize.py
@@ -3,11 +3,16 @@
 from argparse import ArgumentParser, FileType
 from pathlib import Path
 
+import torch
+
 from edgfs2D.utils.dictionary import Dictionary
 from edgfs2D.utils.util import split_vargs
 
 
 def initialize():
+    # do not track gradients globally
+    torch.set_grad_enabled(False)
+
     ap = ArgumentParser()
     sp = ap.add_subparsers(dest="cmd", help="sub-command help")
 
