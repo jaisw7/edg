c4f45b5799aa667cb0efc790fa26fd412812cd40
Shashank Jaiswal <jaisw7@gmail.com>
2025-02-02 06:49:20 -0500

    Remove unused dependencies from setup.py

--- setup.py
+++ setup.py
@@ -68,12 +68,8 @@ package_data = {
 
 # Hard dependencies
 install_requires = [
-    "appdirs >= 1.4.0",
-    "gimmik >= 2.0",
     "h5py >= 2.6",
-    "mako >= 1.0.0",
     "numpy >= 1.8",
-    "pytools >= 2016.2.1",
     "torch >= 2.5.1",
     "protobuf>=4.0.0",
     "isort>=5.13.2",
