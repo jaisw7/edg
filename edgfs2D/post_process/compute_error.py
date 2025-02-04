6d6bbf28a6e9a69e7d2424170a7610fa49f5c90d
Shashank Jaiswal <jaisw7@gmail.com>
2025-02-04 09:24:11 -0500

    Perform error computation on per cell basis

--- edgfs2D/post_process/compute_error.py
+++ edgfs2D/post_process/compute_error.py
@@ -32,22 +32,9 @@ class ComputeErrorPostProcessor(BasePostProcessor):
         file1 = pv.read(self._vf1)
         file2 = pv.read(self._vf2)
 
-        # Check if the number of points is the same
-        if file1.number_of_points != file2.number_of_points:
-            print("The files have different numbers of points")
-        else:
-            if np.abs(file1.points - file2.points).max() > 1e-10:
-                raise RuntimeError("The files have different point coordinates")
-
         # Check if the number of cells is the same
         if file1.number_of_cells != file2.number_of_cells:
-            print("The files have different numbers of cells.")
-        else:
-            if not all(
-                np.array_equal(c1, c2)
-                for c1, c2 in zip(file1.cells, file2.cells)
-            ):
-                raise RuntimeError("The files have different cell connectivity")
+            raise RuntimeError("The files have different numbers of cells.")
 
         # Check if the cell types are the same
         if not np.array_equal(file1.celltypes, file2.celltypes):
@@ -57,7 +44,19 @@ class ComputeErrorPostProcessor(BasePostProcessor):
         if file1.array_names != file2.array_names:
             raise RuntimeError("Data between the two files is not consistent")
 
-        for field in file1.array_names:
-            data1, data2 = file1[field], file2[field]
+        # Get cell data
+        cell_data1 = file1.point_data_to_cell_data()
+        cell_data2 = file2.point_data_to_cell_data()
+
+        def centers(cdata):
+            center = np.array([v.center for v in cdata.cell])
+            np.matrix.sort(center, axis=0)
+            return center
+
+        if np.linalg.norm(centers(cell_data1) - centers(cell_data2)) > 1e-5:
+            raise RuntimeError("Cells between the two files is not consistent")
+
+        for field in cell_data1.array_names:
+            data1, data2 = cell_data1[field], cell_data2[field]
             mse = ((data1 - data2) ** 2).mean()
             logger.info("Field: {}, Mean Squared Error: {}", field, mse)
