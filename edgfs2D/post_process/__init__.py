7b24ab20b90bdedbdfe777b7ea03c27b65b2dfa9
Shashank Jaiswal <jaisw7@gmail.com>
2025-02-04 09:23:18 -0500

    Expose VTU post-processor in module

--- edgfs2D/post_process/__init__.py
+++ edgfs2D/post_process/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from edgfs2D.post_process.base import BasePostProcessor  # noqa
 from edgfs2D.post_process.compute_error import ComputeErrorPostProcessor  # noqa
+from edgfs2D.post_process.vtu import VtuPostProcessor  # noqa
 from edgfs2D.utils.nputil import subclass_where
 
 
