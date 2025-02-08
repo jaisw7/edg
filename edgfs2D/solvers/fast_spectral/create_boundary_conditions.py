fa7a53dfe3412718450c372777259695cc88f1d7
Shashank Jaiswal <jaisw7@gmail.com>
2025-02-08 02:32:18 -0500

    Add specular and diffuse-curved boundary condition

--- edgfs2D/solvers/fast_spectral/create_boundary_conditions.py
+++ edgfs2D/solvers/fast_spectral/create_boundary_conditions.py
@@ -10,6 +10,7 @@ from edgfs2D.solvers.fast_spectral.create_initial_conditions import (
     MaxwellianInitialCondition,
 )
 from edgfs2D.utils.dictionary import SubDictionary
+from edgfs2D.utils.util import fuzzysort
 from edgfs2D.velocity_mesh.base import BaseVelocityMesh
 
 
@@ -71,7 +72,7 @@ class DiffuseWallBoundaryCondition(FastSpectralBoundaryCondition):
             pos,
             neg,
             pos * nu * self.vweights,
-            (neg * nu * self.vweights * self._f0).sum(),
+            -(neg * nu * self.vweights * self._f0).sum(),
         )
 
     @override
@@ -83,7 +84,89 @@ class DiffuseWallBoundaryCondition(FastSpectralBoundaryCondition):
         pos_id, neg_id, pos_val, neg_val = self.trace_ids
 
         # compute number density
-        nden = (pos_val * ul).sum(axis=1) / (-neg_val)
+        nden = (pos_val * ul).sum(axis=1) / neg_val
+
+        # update flux
+        ur = pos_id * ul
+        ur.add_(torch.outer(nden, neg_id * self._f0))
+
+        return ur
+
+
+class SpecularWallBoundaryCondition(FastSpectralBoundaryCondition):
+    kind = "fast-spectral-specular-wall"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        _ = self.trace_ids
+
+    @cached_property
+    def trace_ids(self):
+        """Find inflow and outflow locations"""
+        nl = self.normals
+        if torch.max(torch.abs(nl - nl.mean(dim=0))) > 1e-5:
+            raise ValueError("all normals must be identical")
+
+        Nv = self.vmesh.num_points
+        norm = torch.hstack((nl[0, :], torch.zeros(1, device=self._cfg.device)))
+        cr = self.vpoints - 2 * torch.outer(
+            norm, torch.tensordot(norm, self.vpoints, dims=1)
+        )
+        sorted_idx = torch.arange(0, Nv, device=self._cfg.device)
+        f0 = torch.Tensor(fuzzysort(cr, sorted_idx))
+        if not torch.all(torch.sort(f0)[0] == sorted_idx):
+            raise ValueError("Non-unique map")
+        return f0
+
+    @override
+    def apply(
+        self,
+        curr_time: torch.float64,
+        ul: torch.Tensor,
+    ):
+        idx = self.trace_ids
+        return ul[..., idx]
+
+
+class DiffuseCurvedWallBoundaryCondition(FastSpectralBoundaryCondition):
+    kind = "fast-spectral-diffuse-curved-wall"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        ic = MaxwellianInitialCondition(self._cfg, self._vmesh, read_rho=False)
+        self._f0 = ic.init_vals
+        self._u = ic.u
+        _ = self.trace_ids
+
+    @cached_property
+    def trace_ids(self):
+        """Find inflow and outflow locations"""
+        nl = self.normals
+
+        nu = torch.tensordot(
+            nl, self.vpoints[:2, ...] - self._u[:2, ...], dims=1
+        )
+        print(nu.shape) + exit(0)
+        pos = torch.where(nu >= 0, 1, 0)
+        neg = torch.where(nu < 0, 1, 0)
+        return (
+            pos,
+            neg,
+            pos * nu * self.vweights,
+            -(neg * nu * self.vweights * self._f0).sum(),
+        )
+
+    @override
+    def apply(
+        self,
+        curr_time: torch.float64,
+        ul: torch.Tensor,
+    ):
+        pos_id, neg_id, pos_val, neg_val = self.trace_ids
+
+        # compute number density
+        nden = (pos_val * ul).sum(axis=1) / neg_val
 
         # update flux
         ur = pos_id * ul
